2249d2812a71ca26da44790d8b801e399751d337
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-04 20:22:00 -0600

    query multiples tables

--- schema.sql
+++ schema.sql
@@ -6,8 +6,21 @@ CREATE TABLE animals (
     date_of_birth DATE NOT NULL,
     escape_attempts INTEGER NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
 
  ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+ CREATE TABLE owners (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    full_name VARCHAR(150),
+    age INTEGER
+ );
+
+ CREATE TABLE species (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(150)
+ );
+
+ ALTER TABLE animals DROP COLUMN species;
 
